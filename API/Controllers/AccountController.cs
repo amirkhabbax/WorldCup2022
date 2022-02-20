43fe647b3b208aad07e06ea8f64331f8283609a6
amirkhabbaz <amir.khabbaz@gmail.com>
2022-02-20 10:13:26 +0330

    jwt token service Added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,13 +16,15 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context , ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
             
             if(string.IsNullOrEmpty(registerDto.Password)||
                string.IsNullOrEmpty(registerDto.Username)) return BadRequest("Username or Password is not given");
@@ -39,7 +42,38 @@ namespace API.Controllers
             _context.Users?.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+        
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
+
+            if(string.IsNullOrEmpty(loginDto.Password)||
+               string.IsNullOrEmpty(loginDto.Username)) return BadRequest("Username or Password is not given");
+
+            if(_context.Users == null) return NotFound();
+
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if( user == null || 
+                user.PasswordSalt == null ||
+                user.PasswordHash == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            
+            for(int i=0; i<computedHash.Length;i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username){
